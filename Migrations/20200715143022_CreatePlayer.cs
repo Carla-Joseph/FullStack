4ac0fcd9c39af8fb723178f9564f2fa4d3d50d42
Carla Joseph <carlajseph@gmail.com>
2020-07-16 11:01:50 -0400

    Created sample data

---
+++ Migrations/20200715143022_CreatePlayer.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace FullStack.Migrations
+{
+    public partial class CreatePlayer : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Players",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    PlayerName = table.Column<string>(nullable: true),
+                    TeamName = table.Column<string>(nullable: true),
+                    Position = table.Column<string>(nullable: true),
+                    JerseyNumber = table.Column<int>(nullable: false),
+                    Points = table.Column<int>(nullable: false),
+                    Rebounds = table.Column<int>(nullable: false),
+                    Steals = table.Column<int>(nullable: false),
+                    Blocks = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Players", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Players");
+        }
+    }
+}
