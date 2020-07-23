e9abfec9b4e3b6dab28f240afa3d364820d5be7c
Carla Joseph <carlajseph@gmail.com>
2020-07-23 18:09:10 -0400

    Adding player form validation, submission feedback, and form reset

--- ClientApp/src/pages/AddPlayer.jsx
+++ ClientApp/src/pages/AddPlayer.jsx
@@ -1,11 +1,14 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import './AddPlayer.scss'
 
 export function AddPlayer() {
   let [player, setPlayer] = useState({})
 
   function handleAddPlayer(event) {
     event.preventDefault()
+    const addPlayerForm = document.getElementById('addPlayerForm')
+    addPlayerForm.reportValidity()
     const playerStringified = JSON.stringify(player)
     fetch('https://localhost:5001/api/Players', {
       headers: {
@@ -14,6 +17,11 @@ export function AddPlayer() {
       body: playerStringified,
       method: 'POST',
     })
+      .then(response => response.json())
+      .then(data => {
+        addPlayerForm.reset()
+        alert('Succesfully added player')
+      })
   }
 
   function handlePlayerFieldChanged(fieldName, value) {
@@ -33,114 +41,125 @@ export function AddPlayer() {
       <div className="card">
         <div className="card-header">Add a Player</div>
 
-        <div className="form-group">
-          <input
-            type="text"
-            className="form-control"
-            id="name"
-            placeholder="Player's Name"
-            onChange={event =>
-              handlePlayerFieldChanged('playerName', event.target.value)
-            }
-          />
-        </div>
+        <form id="addPlayerForm">
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control"
+              id="name"
+              placeholder="Player's Name"
+              required
+              onChange={event =>
+                handlePlayerFieldChanged('playerName', event.target.value)
+              }
+            />
+          </div>
 
-        <div className="form-group">
-          <input
-            type="text"
-            className="form-control"
-            id="name"
-            placeholder="Team's Name"
-            onChange={event =>
-              handlePlayerFieldChanged('teamName', event.target.value)
-            }
-          />
-        </div>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control"
+              id="name"
+              placeholder="Team's Name"
+              required
+              onChange={event =>
+                handlePlayerFieldChanged('teamName', event.target.value)
+              }
+            />
+          </div>
 
-        <div className="form-group">
-          <select
-            className="form-control"
-            onChange={event =>
-              handlePlayerFieldChanged('position', event.target.value)
-            }
+          <div className="form-group">
+            <select
+              className="form-control position-selection"
+              onChange={event =>
+                handlePlayerFieldChanged('position', event.target.value)
+              }
+            >
+              <option>PG</option>
+              <option>SG</option>
+              <option>SF</option>
+              <option>PF</option>
+              <option>C</option>
+            </select>
+          </div>
+
+          <div className="form-group">
+            <input
+              type="number"
+              className="form-control"
+              id="name"
+              placeholder="Jersey Number"
+              required
+              onChange={event =>
+                handlePlayerFieldChanged(
+                  'jerseyNumber',
+                  parseInt(event.target.value)
+                )
+              }
+            />
+          </div>
+
+          <div className="form-group">
+            <input
+              type="number"
+              className="form-control"
+              id="name"
+              placeholder="Points"
+              required
+              onChange={event =>
+                handlePlayerFieldChanged('points', parseInt(event.target.value))
+              }
+            />
+          </div>
+
+          <div className="form-group">
+            <input
+              type="number"
+              className="form-control"
+              id="name"
+              placeholder="Rebounds"
+              onChange={event =>
+                handlePlayerFieldChanged(
+                  'rebounds',
+                  parseInt(event.target.value)
+                )
+              }
+            />
+          </div>
+
+          <div className="form-group">
+            <input
+              type="number"
+              className="form-control"
+              id="name"
+              placeholder="Blocks"
+              required
+              onChange={event =>
+                handlePlayerFieldChanged('blocks', parseInt(event.target.value))
+              }
+            />
+          </div>
+
+          <div className="form-group">
+            <input
+              type="number"
+              className="form-control"
+              id="name"
+              placeholder="Steals"
+              required
+              onChange={event =>
+                handlePlayerFieldChanged('steals', parseInt(event.target.value))
+              }
+            />
+          </div>
+
+          <button
+            className="btn btn-primary"
+            onClick={event => handleAddPlayer(event)}
           >
-            <option>PG</option>
-            <option>SG</option>
-            <option>SF</option>
-            <option>PF</option>
-            <option>C</option>
-          </select>
-        </div>
-
-        <div className="form-group">
-          <input
-            type="int"
-            className="form-control"
-            id="name"
-            placeholder="Jersey Number"
-            onChange={event =>
-              handlePlayerFieldChanged(
-                'jerseyNumber',
-                parseInt(event.target.value)
-              )
-            }
-          />
-        </div>
-
-        <div className="form-group">
-          <input
-            type="int"
-            className="form-control"
-            id="name"
-            placeholder="Points"
-            onChange={event =>
-              handlePlayerFieldChanged('points', parseInt(event.target.value))
-            }
-          />
-        </div>
-
-        <div className="form-group">
-          <input
-            type="int"
-            className="form-control"
-            id="name"
-            placeholder="Rebounds"
-            onChange={event =>
-              handlePlayerFieldChanged('rebounds', parseInt(event.target.value))
-            }
-          />
-        </div>
-
-        <div className="form-group">
-          <input
-            type="int"
-            className="form-control"
-            id="name"
-            placeholder="Blocks"
-            onChange={event =>
-              handlePlayerFieldChanged('blocks', parseInt(event.target.value))
-            }
-          />
-        </div>
-
-        <div className="form-group">
-          <input
-            type="number"
-            className="form-control"
-            id="name"
-            placeholder="Steals"
-            onChange={event =>
-              handlePlayerFieldChanged('steals', parseInt(event.target.value))
-            }
-          />
-        </div>
-
-        <button
-          className="btn btn-primary"
-          onClick={event => handleAddPlayer(event)}
-        >
-          Submit
-        </button>
+            Submit
+          </button>
+        </form>
       </div>
     </>
   )
